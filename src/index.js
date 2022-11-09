433b24f6e4ce905611773a92781765fb5029e393
Mario Moreno <mario_morenor@hotmail.com>
2022-11-09 15:08:54 -0500

    Fase de Establecer Comunicación

--- src/index.js
+++ src/index.js
@@ -30,41 +30,42 @@ const io = new Server(httpServer, {
 
 // Conexiones y eventos
 io.on("connection", (socket) => {
-
   //   Devuelve los Managers Conectados
   socket.on("managers_conectados", async (data, response) => {
     const sockets = await io.fetchSockets();
     const managers = sockets
       .filter((s) => s.data.tipo == "manager")
-      .map(s => s.data);
+      .map((s) => s.data);
     response(managers);
   });
 
   socket.on("clientes_conectados", async (data, response) => {
     const sockets = await io.fetchSockets();
     const clientes = sockets
-      .filter(s => s.data.tipo == "cliente")
-      .map(s => s.data)
-    response(clientes)
-  })
-
+      .filter((s) => s.data.tipo == "cliente")
+      .map((s) => s.data);
+    response(clientes);
+  });
 
   // Establece un nuevo Socket
   socket.on("nuevo", (data) => {
-    socket.data = data
-    socket.join(data.department);
+    if (data) {
+      socket.data = data;
+      socket.data.socket_id = socket.id;
+      socket.join(data.department);
 
-    if (data.tipo == "cliente") {
-      io.to(data.department).emit("nuevo_cliente_en_espera", data)
+      if (data.tipo == "cliente") {
+        io.to(data.department).emit("nuevo_cliente_en_espera", data);
+      }
     }
-  })
+  });
 
-  socket.on("disconnecting", () => {
-    io.emit("cliente_desconectado", socket.data.client);
+  socket.on("disconnecting", async () => {
+    const sockets = await io.fetchSockets();
+    const sock = sockets.find((s) => s.id == socket.id);
+    io.to(sock.data.department).emit("socket_desconectado", sock.data);
   });
 });
-
-
 
 httpServer.listen(3000);
 
